b2807e8d92b91a16f0cdbad029f8eb8278f46cd5
Jorge E Trujillo F <joertru@gmail.com>
2023-06-06 16:27:26 -0500

    Creación de las rutas dinamicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Route, Routes, Link } from 'react-router-dom'
+///import { useState } from 'react'
+import { Route, Routes, Link, useParams } from 'react-router-dom'
 import './App.css'
 import 'simpledotcss'
 
@@ -8,8 +8,8 @@ const About = () => <h2>About</h2>
 const Menu = () => {
 
   const menus = [
-    'HotDog',
-    'Hot Potatoes',
+    'Hot_Dog',
+    'Hot_Potatoes',
     'Hamburger'
   ]
 
@@ -19,7 +19,7 @@ const Menu = () => {
       <ul>
         { menus.map( item => (
           <li key={item}>
-            <Link to={`menu/${item}`}>{item}</Link>
+            <Link to={`/menu/${item}`}>{item}</Link>
           </li>
         ))}
       </ul>
@@ -28,7 +28,13 @@ const Menu = () => {
 }
 
 const Items = () => {
-
+  const { name } = useParams()
+  return (
+    <>
+      <h1>Mi menu</h1>
+      { name }
+    </>
+  )
 }
 
 function App() {
@@ -39,14 +45,15 @@ function App() {
         <nav>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/About">About</Link></li>
-          <li><Link to="/Menu">Menu</Link></li>
+          <li><Link to="/menus">Menu</Link></li>
         </nav>
       </header>
       <h1>Rutas</h1>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
-        <Route path='/menu' element={<Menu/>} />
+        <Route path='/menus' element={<Menu/>} />
+        <Route path='/menu/:name' element={<Items/>} />
       </Routes>
     </div>
 
