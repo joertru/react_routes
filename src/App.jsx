2d18fc6ca852b79071fd08d1dacc6a91eef05969
Jorge E Trujillo F <joertru@gmail.com>
2023-06-06 15:56:42 -0500

    Agregando Ajustes de Estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import 'simpledotcss'
 
 const Home = () => <h1>Inicio</h1>
 const About = () => <h2>About</h2>
@@ -9,13 +10,13 @@ function App() {
 
   return (
     <div>
-      <h1>Rutas</h1>
-      <header>
+     <header>
         <nav>
           <li><a href="/">Home</a></li>
           <li><a href="/About">About</a></li>
         </nav>
       </header>
+      <h1>Rutas</h1>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
