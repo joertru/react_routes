1c6e6c7a2f4d176e01b82d9a90c588cef7083a6b
Jorge E Trujillo F <joertru@gmail.com>
2023-06-06 16:19:45 -0500

    Creando menu de navegacion con parametros

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,31 @@ import 'simpledotcss'
 
 const Home = () => <h1>Inicio</h1>
 const About = () => <h2>About</h2>
+const Menu = () => {
+
+  const menus = [
+    'HotDog',
+    'Hot Potatoes',
+    'Hamburger'
+  ]
+
+  return(
+    <>
+      <h1>Menu</h1>
+      <ul>
+        { menus.map( item => (
+          <li key={item}>
+            <Link to={`menu/${item}`}>{item}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+const Items = () => {
+
+}
 
 function App() {
 
@@ -14,12 +39,14 @@ function App() {
         <nav>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/About">About</Link></li>
+          <li><Link to="/Menu">Menu</Link></li>
         </nav>
       </header>
       <h1>Rutas</h1>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
+        <Route path='/menu' element={<Menu/>} />
       </Routes>
     </div>
 
